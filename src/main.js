7b7d1d6c68eb8487d8342e24dc216ec393b11af5
Ezra <ezrakahn1@gmail.com>
2025-07-27 09:43:37 -0700

    Setup google maps api with key injection and config

--- src/main.js
+++ src/main.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const fs = require('fs');
 const dotenv = require('dotenv')
+const path = require('path');
 
-dotenv.config({ path: 'keys.cfg' });
+dotenv.config({ path: 'config/config.cfg' });
+dotenv.config({ path: 'config/keys.cfg' });
 const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
-
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
+
+const publicFolder = path.isAbsolute(process.env.PUBLIC_FOLDER)
+  ? process.env.PUBLIC_FOLDER
+  : path.join(__dirname, process.env.PUBLIC_FOLDER);
+
+console.log(`Using public folder: ${publicFolder}`);
 
 app.get('/', (req, res) => {
-    fs.readFile('index.html', (err, data) => {
+    fs.readFile(path.join(publicFolder, 'index.html'), (err, data) => {
         if (err) {
             res.status(500).send('Error reading file');
             return;
         }
-        res.send(data.toString());
+        const htmlContent = `<script>window.GOOGLE_MAPS_API_KEY = "${GOOGLE_MAPS_API_KEY}";</script>` + data.toString(); // Inject API key
+        res.send(htmlContent);
     });
 });
 
