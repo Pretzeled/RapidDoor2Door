fb2e81b8631d65e6e7d9a3b18ce7754cc545af81
Ezra <ezrakahn1@gmail.com>
2025-07-29 17:47:42 -0700

    Added very insecure auth

--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ const dotenv = require('dotenv')
 const path = require('path');
 const multer = require('multer');
 const upload = multer();
+const cookieParser = require('cookie-parser');
 
 const Database = require('better-sqlite3');
+const { randomBytes } = require('crypto');
 const db = new Database('clients.db');
 db.prepare('CREATE TABLE IF NOT EXISTS clients (placeId TEXT PRIMARY KEY, response TEXT, fname TEXT, lname TEXT, email TEXT, phone TEXT, carMakeAndModel TEXT, notes TEXT, address TEXT, lat REAL, lng REAL)').run();
 
@@ -13,6 +15,7 @@ dotenv.config({ path: 'config/config.cfg' });
 dotenv.config({ path: 'config/keys.cfg' });
 const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
 const app = express();
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 8080;
 
@@ -27,23 +30,45 @@ function respond404(req, res) {
 }
 
 app.post('/api/save', upload.none(), (req, res) => {
+    let authenticated = req.cookies && req.cookies.authkey === authKey; // MAKE SECURE LATER
+    if (!authenticated) {
+        console.log('Unauthorized save request');
+        res.status(401).send('Unauthorized');
+        return;
+    }
+
     console.log('Received save request:', req.body);
     db.prepare('INSERT OR REPLACE INTO clients (placeId, response, fname, lname, email, phone, carMakeAndModel, notes, address, lat, lng) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
     .run(req.body.placeId, req.body.response, req.body.firstName, req.body.lastName, req.body.email, req.body.phoneNumber, req.body.carMakeAndModel, req.body.notes, req.body.address, req.body.lat, req.body.lng);
     res.sendStatus(200);
 });
 
-app.get(/.*/, (req, res) => {
-    let authenticated = true; // Placeholder for authentication logic
-
-    console.log(`Received request for: ${req.path}`);
-    if (req.path == '/api/visits') {
-        const visits = db.prepare('SELECT placeId, lat, lng, response FROM clients').all();
-        res.json(visits);
-        return;
+let authKey = null;
+authKey = randomBytes(64).toString('hex'); // Generate a random auth key
+app.post('/auth', upload.none(), (req, res) => {
+    let username = req.body.username;
+	let password = req.body.password;
+    console.log(`Authentication attempt for user: ${username}`);
+    console.log(`Password: ${password}`);
+    
+    // Placeholder for authentication logic
+    if (username === 'admin' && password === process.env.ADMIN_PASSWORD) {
+        console.log('Authentication successful');
+        authKey = randomBytes(64).toString('hex');
+        res.cookie('authkey', authKey); // Set a cookie for auth key
+        res.redirect('/'); // Redirect to home page after successful authentication
+    } else {
+       res.send('Incorrect Username and/or Password!');
+       res.status(401);
     }
+    res.end();
+});
 
+app.get(/.*/, (req, res) => {
+    console.log(`Received request for: ${req.cookies}`);
+    let authenticated = req.cookies && req.cookies.authkey === authKey; // MAKE SECURE LATER
     let requestedPath = req.path;
+
     if (requestedPath == '/') {
         requestedPath = '/index.html'; // Default to index.html
     }
@@ -52,6 +77,13 @@ app.get(/.*/, (req, res) => {
     if (!authenticated) {
         requestedPath = '/login.html'; // Serve login if not authenticated
     }
+
+    if (requestedPath == '/api/visits') {
+        const visits = db.prepare('SELECT placeId, lat, lng, response FROM clients').all();
+        res.json(visits);
+        return;
+    }
+
     const filePath = path.join(publicFolder, requestedPath);
     // -----  DO NOT EDIT FILE PATH AFTER AUTHENTICATION CHECK -----
     
